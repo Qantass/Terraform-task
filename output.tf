df42ec09ea98ee2e351e1f020adc8dab4f121b30
Qantass <rkiblyk@gmail.com>
2021-06-22 14:42:35 +0300

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,3 +4,12 @@ output "MySQL_server_public_ip" {
 output "TomCat_server_public_ip" {
     value = aws_eip.static_tom.public_ip
 }
+//output "TomCat_server_public_ip" {
+//    value = aws_eip.static_tom.public_ip
+//}
+//output "TomCat_server_public_ip" {
+//    value = aws_eip.static_tom.public_ip
+//}
+//output "TomCat_server_public_ip" {
+//    value = aws_eip.static_tom.public_ip
+//}
