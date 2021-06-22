55c4ef3842d59bc423f0e297da031e2be3782603
Qantass <rkiblyk@gmail.com>
2021-06-22 13:46:37 +0300

    2
    Add Security Group

---
+++ Security_group.tf
@@ -0,0 +1,29 @@
+#------------------ SECURITY GROUP ----------------------------------------
+resource "aws_security_group" "DB" {
+  name = "DB_Security_group"
+  description = "My DB_Security_group"
+
+  dynamic "ingress" {
+      for_each = ["80", "8080", "3360"]
+      content {
+          from_port =ingress.value
+          to_port = ingress.value
+          protocol = "tcp"
+          cidr_blocks = ["0.0.0.0/0"]
+
+      }
+}
+   ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
