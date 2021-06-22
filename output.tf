d6820fdcd94b21d70c9b6ebbcfba3275b8568830
Qantass <rkiblyk@gmail.com>
2021-06-22 16:56:21 +0300

    5
    Add DATA

--- output.tf
+++ output.tf
@@ -4,3 +4,9 @@ output "MySQL_server_public_ip" {
 output "TomCat_server_public_ip" {
     value = aws_eip.static_tom.public_ip
 }
+output "data_aws_availability_zones" {
+    value = data.aws_availability_zones.work.names
+}
+output "data_aws_region_name" {
+    value = data.aws_region.current.name
+}
