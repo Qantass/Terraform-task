e523482a0fafb4c976771b0bc9ad7d617ced2aea
Qantass <rkiblyk@gmail.com>
2021-06-22 14:31:01 +0300

    3
    add Outputs

--- main.tf
+++ main.tf
@@ -3,15 +3,38 @@ provider "aws" {
     profile = "terraform"
     region = "us-east-2"
 }
+#-------------------------EIP -------------------------------
+resource "aws_eip" "static_sql"{
+  instance = aws_instance.mysql.id
+}
+resource "aws_eip" "static_tom"{
+  instance = aws_instance.tomcat.id
+}
+
+#------------------------------------------------------------------
 
 resource "aws_instance" "mysql" {
     ami = "ami-00399ec92321828f5"  # Linux Ubuntu Server 20.04 LTS 
     instance_type = "t2.micro"
+  tags = {
+    Name = "MySQL Server"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_instance" "tomcat" {
     ami = "ami-00399ec92321828f5"      # Linux Ubuntu Server 20.04 LTS
     instance_type = "t2.micro"
+  tags = {
+    Name = "TomCat Server"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 #---------------------- V  P  C -------------------------------
@@ -42,4 +65,3 @@ resource "aws_subnet" "db2" {
     Name = "Tomcat_Sub"
   }
 }
-
